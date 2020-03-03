bc34fbc48ff81b76880fa7386b82295aa095277c
sirwilly1 <kevinawilczynski@gmail.com>
2020-03-02 19:03:45 -0800

    setting up lab2

---
+++ lab2/launch.c
@@ -0,0 +1,13 @@
+#include <time.h>
+#include <sys/times.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char const *argv[]) {
+
+  return 0;
+}

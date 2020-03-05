f90613849fe7a4655fbb50f2d61054de4be52e88
sirwilly1 <kevinawilczynski@gmail.com>
2020-03-04 18:36:36 -0800

    finished lab2 part 1

--- lab2/launch.c
+++ lab2/launch.c
@@ -7,7 +7,45 @@
 #include <stdlib.h>
 
 
-int main(int argc, char const *argv[]) {
 
-  return 0;
+void child_pross(pid_t child_pross);
+void retval_print(int _retval);
+
+int main(int argc, char * argv[]) {
+        pid_t ProssId, retval;
+        int status;
+        //char * ls_args[] = { "/bin/ls" , "-l", NULL};
+
+        ProssId = fork();
+
+        if(argc>=1) {
+
+                if (ProssId < 0) {
+
+                        printf ("%s\n", "FAIL");
+                        exit (EXIT_FAILURE);
+
+                }
+                else if (ProssId == 0) {
+
+                        execv(argv[1], argv+1);
+                        exit(EXIT_SUCCESS);
+
+                }else if(ProssId>0) {
+
+                        retval = waitpid(ProssId, &status, WUNTRACED);
+                        child_pross(ProssId);
+                        retval_print(status);
+                }
+        }
+        return 0;
+}
+
+void child_pross(pid_t child_pross) {
+        fprintf(stderr, "CPID: %d\n", child_pross);
+}
+
+void retval_print(int _retval){
+        fprintf(stderr, "RetVal: %d\n", _retval);
+
 }
